a7d8e7c3cf497dd4898a52e87c237c21966f5c4a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-18 16:11:37 -0800

    ok i think i may be struggling here a while so im going to stop fiddly force pushing

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
+// tslint:disable-next-line:no-reaching-imports
+import houndstoothtopiaTheme from './test/snapshot.json'
+
 export { pattern } from './src'
+
+// tslint:disable-next-line:no-default-export
+export default houndstoothtopiaTheme
