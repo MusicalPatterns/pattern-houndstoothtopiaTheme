e40f8ce24cec2a406e31bf6fb2a15ea2d1fe5c75
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-18 16:45:06 -0800

    remove stupid stuff

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
-// tslint:disable-next-line:no-reaching-imports
-import houndstoothtopiaTheme from './test/snapshot.json'
-
 export { pattern } from './src'
-
-// tslint:disable-next-line:no-default-export
-export default houndstoothtopiaTheme
