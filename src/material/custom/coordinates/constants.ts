91d6b25121de6fdbb511458205b31117ad4c59d7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-02 13:28:00 -0800

    missing tslint disable

--- src/material/custom/coordinates/constants.ts
+++ src/material/custom/coordinates/constants.ts
@@ -1,3 +1,5 @@
+// tslint:disable no-magic-numbers
+
 import { negative, to, Translation } from '@musical-patterns/utilities'
 
 const TRANSLATION_TO_SPECIALIZE_COORDINATES_FOR_THEME: Translation = to.Translation(negative(1))
